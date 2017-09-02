07e982a364c2d8cc92073638466fbe4cc7e07481
Vasiliy Alferov <ya-ikmik2012@yandex.ru>
2017-09-02 17:28:53 +0300

    add some links

--- rgpsite/rgpmon/urls.py
+++ rgpsite/rgpmon/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.PlayerListView.as_view(), name='index'),
-    url(r'^alivers$', views.AlivePlayerListView.as_view(), name='alivers')
+    url(r'^alivers$', views.AlivePlayerListView.as_view(), name='alivers'),
+    url(r'^population/(?P<population>.+)/alivers', views.population_alivers, name='population_alivers'),
 ]
--- rgpsite/rgpmon/views.py
+++ rgpsite/rgpmon/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 
-from .models import Launch, Player
+from .models import *
 
 # Create your views here.
 
@@ -20,3 +20,15 @@ class AlivePlayerListView(PlayerListView):
     def get_queryset(self):
         l = list(Launch.objects.all())[0]
         return Player.objects.filter(g_end=l.last_generation).order_by('-elo')
+
+
+def population_alivers(request, population):
+    template_name='rgpmon/alivers.html'
+
+    l = list(Launch.objects.all())[0]
+    p = list(Population.objects.filter(name=population))[0]
+    
+    player_list = Player.objects.filter(g_end=l.last_generation,
+                                        population=p).order_by('-elo')
+    return render(request, template_name, { 'player_list': player_list })
+    
